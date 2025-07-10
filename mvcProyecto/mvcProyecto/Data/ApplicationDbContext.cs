06105e83bfdbe228dd29b2d1b56f694777e25ab7
AdrianaRodriguez7 <137732661+AdrianaRodriguez7@users.noreply.github.com>
2025-07-10 19:21:44 -0400

    agregando el proyecto mvc parte I

---
+++ mvcProyecto/mvcProyecto/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    //Aqui todos los modelos que se creen 
+    public DbSet<Producto> Producto { get; set; }
+}
+
